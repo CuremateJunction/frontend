daccf19c914c965bc5a5142afc05505e71fceffa
Sofia d'Atri <datris@usi.ch>
2023-11-11 20:01:44 +0100

    Component
    Created the SettingList component, that holds the menu of the Profile/Settings page

---
+++ src/components/SettingsList.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-expansion-panels variant="accordion" class="mb-6 flex-grow-1" v-for="category in categories" :key="category.id">
+
+    <v-expansion-panel
+      :title="category.name"
+      text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima"
+    >
+    </v-expansion-panel>
+  </v-expansion-panels>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      categories: [
+        {
+          id: 1,
+          name: 'Profile',
+        },
+        {
+          id: 2,
+          name: 'Doctor',
+        },
+        {
+          id: 3,
+          name: 'Settings',
+        },
+        {
+          id: 4,
+          name: 'Help',
+        },
+      ],
+    };
+  },
+};
+
+</script>
+
+<style scoped>
+
+</style>
