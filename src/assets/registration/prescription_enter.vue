0f1ce3d321824799347b374e07994c5b59f00ffb
SirVectrex <florian.schulenberg@web.de>
2023-11-11 12:35:23 +0200

    Registration "ongoing"

---
+++ src/assets/registration/prescription_enter.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>Please add your prescription.</template>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,14 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import('@/views/Home.vue'),
       },
+      {
+        path: 'registration',
+        name: 'Registration',
+        // route level code-splitting
+        // this generates a separate chunk (Home-[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('@/views/Registration.vue'),
+      },
     ],
   },
 ];
