32f11143e3f0cfc67b9a0695864c6258b219b0aa
Sofia d'Atri <datris@usi.ch>
2023-11-11 20:02:29 +0100

    View
    Created the view for the Profile/Settings page.

---
+++ src/views/ProfilePage.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container
+    class="d-flex flex-column align-center ma-0 pa-0 justify-center h-100">
+    <WaveTop class="d-flex mt-0 pt-0 pl-0 w-100" />
+
+    <v-row class="d-flex align-center justify-center">
+      <v-col cols="auto"> PROFILE </v-col>
+    </v-row>
+
+    <SettingsList />
+
+    <v-row class="d-flex flex-column flex-grow-1">
+      <v-col cols="auto">
+
+      </v-col>
+    </v-row>
+
+    <WaveBottom class="mt-auto w-100" />
+  </v-container>
+</template>
+
+
+<script>
+import {defineComponent} from "vue";
+import WaveBottom from "@/components/WaveBottom.vue";
+import WaveTop from "@/components/WaveTop.vue";
+import SettingsList from "@/components/SettingsList.vue";
+
+export default defineComponent({
+  components: {WaveTop, WaveBottom, SettingsList},
+  props: {
+    username: {
+      type: String,
+      default: 'Lisa',
+    },
+  },
+})
+</script>
+<style scoped>
+
+</style>
