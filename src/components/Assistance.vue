ebe85651ec44244955e5c9c308dd2c62ab04a163
Jeferson Morales Mariciano <jeferson.morales.mariciano@gmail.com>
2023-11-11 21:22:28 +0100

    fix: help page
    Signed-off-by: Jeferson Morales Mariciano <jeferson.morales.mariciano@gmail.com>

--- src/components/Assistance.vue
+++ src/components/Assistance.vue
@@ -4,7 +4,9 @@
   <h3>Need help?</h3>
   <div class="flex container">
     <div class="col-md-12 text-center">
-      <router-link to="/help" class="btn btn-info btn-lg">Call a doctor</router-link>
+      <router-link to="/help" class="btn btn-info btn-lg"
+        >Call a doctor</router-link
+      >
       <button class="btn btn-info btn-lg">Join Finn</button>
     </div>
   </div>
--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,100 +1,94 @@
-<script setup></script>
-
 <template>
-  <div id="item" class="content">
+  <v-container
+    class="d-flex flex-column align-center ma-0 pa-0 justify-center h-100"
+    fluid>
+    <WaveTop class="d-flex mt-0 pt-0 pl-0 w-100" />
+
     <!-- This page serves the purpose of saying hello to a new user before he gets to enter his details -->
-    <div id="title">
-      <div class="text-h5 w-50">
-        Emergency <br />
-        <div id="finn">Contacts</div>
+    <v-row class="d-flex flex-column">
+      <div id="title" class="d-flex text-h5 font-weight-light mb-n1">
+        Emergency
       </div>
-    </div>
+      <h1 class="d-flex text-h2 font-weight-bold">Contacts</h1>
+    </v-row>
 
-    <div class="d-flex align-center flex-column cards">
-      <v-card
-        class="mx-auto card my-4"
-        width="400"
-        title="Your doctor"
-        text="Dr. Med. Stefanson is available from 8am to 6pm"
-        prepend-icon="mdi-doctor"
-        append-icon="mdi-phone-dial"
-        href="tel:+49123-456-7890"
-        target="_blank"
-        rel="noopener"></v-card>
+    <v-row class="d-flex flex-column justify-start">
+      <v-col cols="auto">
+        <v-card
+          color="primary"
+          title="Your Doctor"
+          text="Dr. Med. Stefanson is available from 8am to 6pm"
+          prepend-icon="mdi mdi-doctor"
+          append-icon="mdi mdi-phone-dial"
+          href="tel:+49123-456-7890"
+          target="_blank"
+          rel="noopener">
+        </v-card>
+      </v-col>
 
-      <v-card
-        class="mx-auto card my-4"
-        width="400"
-        title="Speak with a psychologist."
-        text="Hotline is available 24/7"
-        prepend-icon="mdi-doctor"
-        append-icon="mdi-phone-dial"
-        href="tel:+49123-456-7890"
-        target="_blank"
-        rel="noopener"></v-card>
+      <v-col cols="auto">
+        <v-card
+          color="primary"
+          title="Talk to a psychologist"
+          text="Hotline is available 24/7"
+          prepend-icon="mdi mdi-doctor"
+          append-icon="mdi mdi-phone-dial"
+          href="tel:+49123-456-7890"
+          target="_blank"
+          rel="noopener">
+        </v-card>
+      </v-col>
 
-      <v-card
-        class="mx-auto card my-4"
-        width="400"
-        title="Speak with a pharmacist."
-        text="Nearby pharmacy 1.2 km away - open 24/7"
-        prepend-icon="mdi-pill"
-        append-icon="mdi-navigation-variant"
-        href="http://maps.google.com/?q=Yliopiston Apteekki, Mannerheimintie 96, Helsinki"
-        target="_blank"
-        rel="noopener"></v-card>
+      <v-col cols="auto">
+        <v-card
+          color="primary"
+          title="Talk to a pharmacist"
+          text="Nearby pharmacy 1.2 km away - open 24/7"
+          prepend-icon="mdi mdi-pill"
+          append-icon="mdi mdi-navigation-variant"
+          href="http://maps.google.com/?q=Yliopiston Apteekki, Mannerheimintie 96, Helsinki"
+          target="_blank"
+          rel="noopener"></v-card>
+      </v-col>
 
-      <v-card
-        class="mx-auto card my-4"
-        width="400"
-        title="Elisa "
-        text="Your family contact."
-        prepend-icon="mdi-home-heart"
-        append-icon="mdi-phone-dial"
-        href="tel:+49123-456-7890"
-        target="_blank"
-        rel="noopener"></v-card>
+      <v-col cols="auto">
+        <v-card
+          color="primary"
+          title="Elisa"
+          text="Your family contact."
+          prepend-icon="mdi mdi-home-heart"
+          append-icon="mdi mdi-phone-dial"
+          href="tel:+49123-456-7890"
+          target="_blank"
+          rel="noopener"></v-card>
+      </v-col>
+    </v-row>
 
+    <v-row class="d-flex align-start justify-center">
       <v-alert
-        class="mx-auto card my-4"
+        color="warning"
         density="compact"
         type="warning"
-        text="In case of an emergency, please call 112."></v-alert>
-    </div>
+        style="height: 3rem"
+        text="In case of an emergency, please call 112." />
+    </v-row>
 
-    <a class="link" href="/dashboard"><v-icon icon="mdi-home"></v-icon>Home</a>
-  </div>
+    <v-row class="d-flex align-start justify-center">
+      <v-btn icon="mdi mdi-home" text="Home"></v-btn>
+    </v-row>
+  </v-container>
 </template>
 
-<style scoped>
-.content {
-  padding-left: 20px;
-  padding-top: 40px;
-  background-image: url('@/assets/header.svg');
-  background-size: 100vw;
-}
+<script>
+import WaveTop from '@/components/WaveTop.vue';
+import WaveBottom from '@/components/WaveBottom.vue';
 
-#title {
-  font-weight: lighter;
-}
+export default {
+  components: {
+    WaveTop,
+    WaveBottom,
+  },
+};
+</script>
 
-#finn {
-  font-weight: bold;
-  padding-top: 20px;
-  font-size: 80px;
-}
-
-.card {
-  /* margin-bottom: 20px; */
-  background-color: #38e3d1;
-}
-
-.cards {
-  margin-top: 80px;
-}
-
-.link {
-  text-decoration: none;
-  color: #000000;
-}
-</style>
+<style scoped></style>
