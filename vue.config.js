4d35313b3bc1b5e8549db1eec38eba58aa9dd2ec
Jeferson Morales Mariciano <jeferson.morales.mariciano@gmail.com>
2023-11-11 11:41:50 +0100

    chore: run style:fix
    Signed-off-by: Jeferson Morales Mariciano <jeferson.morales.mariciano@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2023 The Pufferfish Team
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 const { defineConfig } = require('@vue/cli-service');
 const { VuetifyPlugin } = require('webpack-plugin-vuetify');
 
